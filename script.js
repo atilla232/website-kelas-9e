6ca2f7f230754f14139e71a22e375cfdb94f5857
atilla232 <135022355+atilla232@users.noreply.github.com>
2023-09-16 21:30:57 +0700

    update js (alpha 0.1)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Dapatkan nama folder dari URL
+const pathArray = window.location.pathname.split("/");
+const folderName = pathArray[pathArray.length - 2];
 
+// Setel judul dengan nama folder
+const titleElement = document.getElementById("dynamicTitle");
+if (titleElement) {
+  titleElement.textContent = folderName;
+}
